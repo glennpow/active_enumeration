4b7fc3118061f3be3ee7189412e926bc5c3aeb1e
glennpow <glennpow@gmail.com>
2008-12-31 09:50:58 -0800

    Fixed helper

--- lib/active_enumeration.rb
+++ lib/active_enumeration.rb
@@ -1,3 +1,6 @@
 require File.dirname(__FILE__) + '/active_enumeration/base'
 require File.dirname(__FILE__) + '/active_enumeration/enumeration_reflection'
 require File.dirname(__FILE__) + '/active_enumeration/has_enumeration'
+require File.dirname(__FILE__) + '/active_enumeration/enumeration_helper'
+
+ActionView::Base.send :include, EnumerationHelper
